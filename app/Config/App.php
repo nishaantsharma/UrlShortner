15cd977743c612b4086a590b54c1cacc36067063
Nishant Sharma <61536117+nishaantsharma@users.noreply.github.com>
2022-10-30 23:57:42 +0530

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://project_new.com';
+	public $baseURL = '';
 
 	/*
 	|--------------------------------------------------------------------------
